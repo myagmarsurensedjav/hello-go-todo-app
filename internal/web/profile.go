64abc4d3705e842efd59170ed918c9410c18c837
Mygmarsuren Sedjav <miigaa.sv@gmail.com>
2023-01-26 19:04:03 +0800

    update profile page

--- internal/web/profile.go
+++ internal/web/profile.go
@@ -1,9 +1,7 @@
 package web
 
 import (
-	"encoding/json"
 	"hello-go-todo-app/internal/auth"
-	"hello-go-todo-app/internal/task"
 	"hello-go-todo-app/internal/user"
 	template "html/template"
 	"net/http"
@@ -18,17 +16,7 @@ func ShowProfile(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 
-	uJson, _ := json.Marshal(u)
-
-	data, err := task.GetTasksCountByLast15Days(auth.GetUserId(r))
-
-	if err != nil {
-		panic(err)
-	}
-
 	tmpl.ExecuteTemplate(w, "base", map[string]interface{}{
-		"User":     u,
-		"UserJson": string(uJson),
-		"Data":     data,
+		"user": u,
 	})
 }
